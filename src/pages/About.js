61d3d3d213da1e234a6f2a59d40a95d4954ac397
jettb2 <jettblaschka@gmail.com>
2023-04-05 15:56:39 +0200

    updated text on home page and about us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,9 +11,9 @@ export default function About() {
     return (
         <div className="About">
             <h2>About Us</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
+            <p>Our mission is to help you find the job of your dreams with ease and convenience. We know that job hunting can be challenging, and that is why we have created a platform that is both efficient and effective. We offer a wide range of tools and resources to help you find the perfect job, from personalized job alerts to expert career advice.</p>
+            <p>At Jobify, we are dedicated to providing you with the best possible experience on our platform. Our team of experts is constantly updating our job listings to ensure that you have access to the latest job opportunities in your field. We believe in the power of technology to connect job seekers with employers, and we are proud to offer a state-of-the-art platform that is easy to use and navigate. With Jobify, you can take your career to new heights.</p>
+            <p>Our commitment to our users extends beyond just helping you find a job. We are also here to provide support and guidance throughout your job search journey. From resume writing tips to interview coaching, we offer a wide range of resources to help you succeed. Our ultimate goal is to empower you with the knowledge and tools you need to land your dream job. Thank you for choosing Jobify, and we look forward to helping you achieve your career goals.</p>
 
             <button onClick={() => setUser(null)}>Logout</button>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,9 @@ export default function Home() {
     return (
         <div className="home">
             <h2> Welcome </h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
+            <p>Welcome to our job search website! We are thrilled to have you here and look forward to helping you in your job search journey. Our website is designed to provide you with a simple and efficient way to find job opportunities that match your skills and preferences. With a user-friendly interface and a wide range of job listings from various industries, we aim to make your job search experience as seamless as possible.</p>
+            <p>At our job search website, we understand that finding a job can be a daunting task. That is why we offer a range of features and tools to help you along the way. Our job options allow you to see a variety of jobs based on location, industry, job title and salary.</p>
+            <p>We are committed to providing you with the best possible experience on our job search website. Our team is dedicated to keeping our job listings up to date and ensuring that our website is user-friendly and accessible to all. We encourage you to explore our website, browse through our job listings, and take advantage of our resources. Thank you for choosing our job search website, and we wish you the best of luck in your job search!</p>
         </div>
     )
 }
